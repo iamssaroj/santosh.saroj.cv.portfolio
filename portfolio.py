a6149030c9cca593e0bcb71bd689f9431840e768
iamssaroj <158077992+iamssaroj@users.noreply.github.com>
2025-04-26 17:38:08 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -4,7 +4,8 @@ import streamlit as st
 st.title("👨‍💻 Santosh Hariram Saroj - Portfolio")
 
 st.header("About Me")
-st.write("Data Science Enthusiast passionate about Machine Learning and Python.")
+st.write("Aspiring data analyst with a background in finance and mathematics. Currently pursuing a Master’s in Management Studies (Finance) and certified in business analysis and financial accounting. 
+Experienced in teaching mathematics with strong analytical and problem-solving skills. Looking to apply data skills to support business insights and decision-making.")
 
 st.header("Projects")
 st.subheader("1. Customer Churn Prediction")
@@ -12,11 +13,12 @@ st.write("Predicts which customers are likely to leave.")
 
 st.header("Skills")
 st.write("""
-- Python
-- SQL
-- Machine Learning
-- Power BI
+- ✅ Python
+- 📊 SQL
+- 🤖 Machine Learning
+- 📈 Power BI
+- 🌐 Streamlit
 """)
 
 st.header("Contact Me")
-st.write("Email: santosh@example.com | GitHub: github.com/santosh")
+st.write("Email: sarojsantosh5@.com | phone: 8898552361 | GitHub: github.com/santosh")
