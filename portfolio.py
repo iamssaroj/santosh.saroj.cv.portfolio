10bd76a415c32b5365acea1b709a3fef31ffcfc8
iamssaroj <158077992+iamssaroj@users.noreply.github.com>
2025-04-26 17:38:38 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.title("👨‍💻 Santosh Hariram Saroj - Portfolio")
 
 st.header("About Me")
-st.write("Aspiring data analyst with a background in finance and mathematics. Currently pursuing a Master’s in Management Studies (Finance) and certified in business analysis and financial accounting. 
-Experienced in teaching mathematics with strong analytical and problem-solving skills. Looking to apply data skills to support business insights and decision-making.")
+st.write(" Aspiring data analyst with a background in finance and mathematics. Currently pursuing a Master’s in Management Studies (Finance) and certified in business analysis and financial accounting. 
+Experienced in teaching mathematics with strong analytical and problem-solving skills. Looking to apply data skills to support business insights and decision-making. ")
 
 st.header("Projects")
 st.subheader("1. Customer Churn Prediction")
