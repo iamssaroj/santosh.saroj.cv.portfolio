56ef278c6f06a59cd4c305929b56d3f2632a13cb
iamssaroj <158077992+iamssaroj@users.noreply.github.com>
2025-04-26 17:30:39 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -11,10 +11,12 @@ st.subheader("1. Customer Churn Prediction")
 st.write("Predicts which customers are likely to leave.")
 
 st.header("Skills")
-st.write("- Python
+st.write(\"\"\"
+- Python
 - SQL
 - Machine Learning
-- Power BI")
+- Power BI
+\"\"\")
 
 st.header("Contact Me")
 st.write("Email: santosh@example.com | GitHub: github.com/santosh")
