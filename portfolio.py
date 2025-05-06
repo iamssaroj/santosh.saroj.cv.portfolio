bda6a3aff6145964ee072c4f7e365bee03fbc6f9
iamssaroj <158077992+iamssaroj@users.noreply.github.com>
2025-05-06 11:13:16 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -45,9 +45,9 @@ st.markdown("""
 # Title and Image - About Me Section
 col1, col2 = st.columns([1, 3])
 with col1:
-    st.image("image.jpg", caption="Santosh Anjali Hariram Saroj", width=150)
+    st.image("image.jpg", caption="Santosh", width=150)
 with col2:
-    st.title("SANTOSH HARIRAM SAROJ")
+    st.title("SANTOSH ANJALI HARIRAM SAROJ")
     st.write("""
     Aspiring data analyst with a background in finance and mathematics. 
     Currently pursuing a Master’s in Management Studies (Finance) and certified 
